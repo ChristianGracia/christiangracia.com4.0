f4a8816f76d34e86e306e71137a617c28e1662c6
Christian Gracia <christiangracia.student@careerdevs.com>
2021-09-16 20:46:08 -0500

    changed use of scss to css, added header and sidebar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SideBarComponent } from './navigation/side-bar/side-bar.component';
+import { HeaderComponent } from './navigation/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, SideBarComponent],
+  declarations: [AppComponent, HomePageComponent, SideBarComponent, HeaderComponent],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     BrowserAnimationsModule,
--- src/app/navigation/side-bar/side-bar.component.ts
+++ src/app/navigation/side-bar/side-bar.component.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
-  styleUrls: ['./side-bar.component.css'],
+  styleUrls: ['./side-bar.component.scss'],
 })
 export class SideBarComponent implements OnInit {
   @Output() closeSidenavEvent = new EventEmitter<void>();
