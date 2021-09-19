9f5bd590585c439caf8e42a510ba308963ef2ecc
Christian Gracia <christiangracia.student@careerdevs.com>
2021-09-19 04:20:07 -0500

    projects component more set up;

--- src/app/pages/projects/github-repos/github-repos.component.ts
+++ src/app/pages/projects/github-repos/github-repos.component.ts
@@ -13,7 +13,7 @@ export class GithubReposComponent implements OnInit {
   public gitRepos: Repo[] = [];
   public data: Repo[] = [];
   public page = 0;
-  public size = 5;
+  public size = 4;
 
   constructor(private githubService: GithubService, public dialog: MatDialog) {}
 
