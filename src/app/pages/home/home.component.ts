171e765411a605d85db1f3e9ce0d047798b461b0
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2021-12-12 21:09:59 -0600

    added back social media links

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LocationData } from 'src/app/models/location-data.model';
+import { EmailService } from 'src/app/services/email.service';
+import { LocationService } from 'src/app/services/location.service';
 import { RoutingService } from '../../services/routing.service';
 @Component({
   selector: 'app-home',
@@ -6,11 +9,16 @@ import { RoutingService } from '../../services/routing.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private routingService: RoutingService) {}
+  constructor(private routingService: RoutingService, private locationService: LocationService, private emailService: EmailService) {}
 
   ngOnInit(): void {}
 
   public goToAboutPage(): void {
+    this.locationService
+    .getLocationJSON()
+    .subscribe((locationData: LocationData) => {
+      this.emailService.sendSiteVisitEmail(locationData).subscribe(() => {});
+    });
     this.routingService.navigateToAbout();
   }
 }
