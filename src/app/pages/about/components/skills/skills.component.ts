72ae44d51f03efb6d3d7bf152fa2810543be370b
Christian Gracia <christiangracia.student@careerdevs.com>
2021-12-13 00:59:50 -0600

    refactored rendering of skills component into a loop

--- src/app/pages/about/components/skills/skills.component.ts
+++ src/app/pages/about/components/skills/skills.component.ts
@@ -1,52 +1,57 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 const LANGUAGES = [
-  "JavaScript",
-  "Java",
-  "Python",
-  "C#",
-  "Ruby",
-  "Swift",
-  "C",
-  "Objective-C",
-  "TypeScript",
-  "HTML/CSS + Sass",
+  'JavaScript',
+  'Java',
+  'Python',
+  'C#',
+  'Ruby',
+  'Swift',
+  'C',
+  'Objective-C',
+  'TypeScript',
+  'HTML/CSS + Sass',
 ];
 
 const FRONTEND_SKILLS = [
-  "React",
-  "Angular",
-  "React Native",
-  "Mobile iOS/Android",
-  "Bootstrap",
-  "Material Design",
-  "Vue.js",
-  "Tailwind CSS",
-  "ASP.NET",
-  "Java Web Apps",
+  'React',
+  'Angular',
+  'React Native',
+  'Mobile iOS/Android',
+  'Bootstrap',
+  'Material Design',
+  'Vue.js',
+  'Tailwind CSS',
+  'ASP.NET',
+  'Java Web Apps',
 ];
 
 const BACKEND_SKILLS = [
-  "RESTful APIs",
-  "NodeJS",
-  "Spring Boot",
-  "Ruby on Rails",
-  "GraphQL",
-  "Flask",
-  "Django",
-  "AWS",
-  "GCP",
+  'RESTful APIs',
+  'NodeJS',
+  'Spring Boot',
+  'Ruby on Rails',
+  'GraphQL',
+  'Flask',
+  'Django',
+  'AWS',
+  'GCP',
+];
+
+const SKILLS: { [key: string]: any } = [
+  { languages: [LANGUAGES.join(', ')] },
+  { 'front-end': [FRONTEND_SKILLS.join(', ')] },
+  { 'back-end': [BACKEND_SKILLS.join(', ')] },
 ];
 
 @Component({
-  selector: "app-skills",
-  templateUrl: "./skills.component.html",
-  styleUrls: ["./skills.component.scss"],
+  selector: 'app-skills',
+  templateUrl: './skills.component.html',
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent implements OnInit {
-  public languagesArray: string[] = [LANGUAGES.join(", ")];
-  public frontEndArray: string[] = [FRONTEND_SKILLS.join(", ")];
-  public backEndArray: string[] = [BACKEND_SKILLS.join(", ")];
+  public skills = SKILLS;
+  public skillKeys: string[] = ['languages', 'front-end', 'back-end'];
 
   constructor() {}
 
