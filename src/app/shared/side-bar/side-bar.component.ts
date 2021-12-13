63ded0739a96dd9e102ecb51744f8bb7c2f2e03b
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2021-12-12 22:19:00 -0600

    made side bar links render dynamically

--- src/app/shared/side-bar/side-bar.component.ts
+++ src/app/shared/side-bar/side-bar.component.ts
@@ -1,16 +1,28 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { RoutingService } from 'src/app/services/routing.service';
 
+const LINKS = {
+  home: "/",
+  about: "/about",
+  projects: "/projects",
+  contact: "/contact",
+}
 @Component({
   selector: 'app-side-bar',
   templateUrl: './side-bar.component.html',
   styleUrls: ['./side-bar.component.scss'],
 })
 export class SideBarComponent implements OnInit {
+  public links = Object.keys(LINKS);
   @Output() private closeSidenavEvent = new EventEmitter<void>();
-  constructor() {}
+  constructor(private routingService: RoutingService) {}
   ngOnInit(): void {}
 
   public onClose() {
     this.closeSidenavEvent.emit();
+  }
+
+  public goToLink(page: string) : void {
+    this.routingService.navigateToPage(page !== 'home' ? page : '/');
   }
 }
