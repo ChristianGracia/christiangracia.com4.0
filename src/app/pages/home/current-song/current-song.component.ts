6d101ff69d24d237d7e029dc82d6198e6b0f0310
Christian Gracia <christiangracia.student@careerdevs.com>
2021-12-15 20:03:12 -0600

    added to switch laptops

--- src/app/pages/home/current-song/current-song.component.ts
+++ src/app/pages/home/current-song/current-song.component.ts
@@ -5,36 +5,48 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 @Component({
   selector: 'app-current-song',
   templateUrl: './current-song.component.html',
-  styleUrls: ['./current-song.component.scss']
+  styleUrls: ['./current-song.component.scss'],
 })
 export class CurrentSongComponent implements OnInit {
   public song: Song | null = null;
   public loadingSong: Boolean = false;
-  constructor(private spotifyService: SpotifyService) { }
+  constructor(private spotifyService: SpotifyService) {}
   public songProgress = 0;
 
   ngOnInit(): void {
     setTimeout(() => {
       this.getCurrentSong();
-    }, 500)
+    }, 500);
   }
 
   private getCurrentSong(): void {
-    this.loadingSong = true;
-    this.spotifyService.getCurrentSong().subscribe(
-      (song: Song[]) => {
-        console.log(song);
-        if (song.length) {
-          this.song = song[0];
-          this.songProgress = (this.song.progress / this.song.duration) * 100
-        }
-        this.loadingSong = false;
-      },
-      (err) => {
-        this.loadingSong = false;
-        console.log(err);
-      }
-    );
-  }
+    this.song = {
+      progress: 232069,
+      duration: 242965,
+      artist: 'Drake',
+      name: 'Knife Talk (with 21 Savage ft. Project Pat)',
+      images: [
+        'https://i.scdn.co/image/ab67616d0000b273cd945b4e3de57edd28481a3f',
 
+        'https://i.scdn.co/image/ab67616d00001e02cd945b4e3de57edd28481a3f',
+
+        'https://i.scdn.co/image/ab67616d00004851cd945b4e3de57edd28481a3f',
+      ],
+    };
+    this.loadingSong = false;
+    // this.spotifyService.getCurrentSong().subscribe(
+    //   (song: Song[]) => {
+    //     console.log(song);
+    //     if (song.length) {
+    //       this.song = song[0];
+    //       this.songProgress = (this.song.progress / this.song.duration) * 100
+    //     }
+    //     this.loadingSong = false;
+    //   },
+    //   (err) => {
+    //     this.loadingSong = false;
+    //     console.log(err);
+    //   }
+    // );
+  }
 }
