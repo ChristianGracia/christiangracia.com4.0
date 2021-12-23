3cb3dd828e6f09f59e54528227beba6543f5992f
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2021-12-23 11:27:46 -0600

    added some more current projects

--- src/app/pages/about/components/side-projects/side-projects.component.ts
+++ src/app/pages/about/components/side-projects/side-projects.component.ts
@@ -1,16 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Site } from '../../../../models/site.model';
 
+const CG: Site = new Site(
+  'Christiangracia.com',
+  'Rewrote this site for the 4th time and there are still some unfinished things on this rewrite',
+  'www.github.com/ChristianGracia/christiangracia.com4.0'
+);
+
+const ALGORITHMS: Site = new Site(
+  'Data Structures and Algorithms',
+  'Doing review of Data structures and Algorithms and work done on this will be seen here',
+  'www.github.com/ChristianGracia/Algorithms'
+);
+
 const NFL_SITE: Site = new Site(
   "Nature's Frontier Landscaping",
   'Rewritting this site in TypeScript + adding server side rendering. I made this site when I first started learning React and want to redo + redesign',
-  'https://www.nflandscaping.com'
+  'www.nflandscaping.com'
 );
 
 const MARKETFEELS: Site = new Site(
   'MarketFeels',
   'Working on the ability to view finance info, post on social medias using bots (hype), add machine learning models, etc',
-  'https://marketfeels.com'
+  'www.marketfeels.com'
 );
 
 @Component({
@@ -19,7 +31,7 @@ const MARKETFEELS: Site = new Site(
   styleUrls: ['./side-projects.component.scss'],
 })
 export class SideProjectsComponent implements OnInit {
-  public sites: Site[] = [MARKETFEELS, NFL_SITE];
+  public sites: Site[] = [CG, ALGORITHMS, MARKETFEELS, NFL_SITE];
 
   constructor() {}
 
