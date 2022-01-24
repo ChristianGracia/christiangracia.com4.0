f9240166e258b6d440133750369892d700694a0a
Christian Gracia <christiangracia.student@careerdevs.com>
2022-01-23 21:38:11 -0600

    lots of design improvements

--- src/app/pages/projects/modals/view-site-modal/view-site-modal.component.ts
+++ src/app/pages/projects/modals/view-site-modal/view-site-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Inject } from "@angular/core";
-import { MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { SafePipe } from '../../services/safe.pipe';
 
 export interface DialogData {
   repo: string;
@@ -7,23 +8,26 @@ export interface DialogData {
 }
 
 @Component({
-  selector: "app-view-site-modal",
-  templateUrl: "./view-site-modal.component.html",
-  styleUrls: ["./view-site-modal.component.scss"],
+  selector: 'app-view-site-modal',
+  templateUrl: './view-site-modal.component.html',
+  styleUrls: ['./view-site-modal.component.scss'],
 })
 export class ViewSiteModalComponent implements OnInit {
   public iframeLoading = true;
   constructor(@Inject(MAT_DIALOG_DATA) public data: DialogData) {}
 
   ngOnInit() {
-    const iframe = document.getElementById("site-iframe");
+    const iframe = document.getElementById('site-iframe');
     if (iframe) {
-      iframe.addEventListener("load", this.iframeLoaded.bind(this), {
+      iframe.addEventListener('load', this.iframeLoaded.bind(this), {
         passive: true,
       });
     }
   }
 
+  public openSite(url: string) {
+    window.open(url, '_blank');
+  }
   private iframeLoaded() {
     this.iframeLoading = false;
   }
