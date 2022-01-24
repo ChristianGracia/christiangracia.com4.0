e85b100c30265e46fa28ae6d6cc62033b911671d
Christian Gracia <christiangracia.student@careerdevs.com>
2022-01-23 23:21:27 -0600

    added interface

--- src/app/pages/about/component/site/site.component.ts
+++ src/app/pages/about/component/site/site.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Section {
+  title: string;
+  description: string[];
+  image: string[];
+}
+
+const sections = [{}];
 @Component({
   selector: 'app-site',
   templateUrl: './site.component.html',
-  styleUrls: ['./site.component.scss']
+  styleUrls: ['./site.component.scss'],
 })
 export class SiteComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
