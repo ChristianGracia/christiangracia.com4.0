3e8343edd6baea0ad6bec387f6d8e819ef666b19
Christian Gracia <42984214+ChristianGracia@users.noreply.github.com>
2022-05-15 12:51:09 -0400

    Big refactor (#34)
    * feat: big refactor, hosting own font and material icons now

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,17 +16,13 @@ import { RoutingService } from "../../services/routing.service";
   styleUrls: ["./home.component.scss"],
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  // @ViewChild("currentSongComponent", { read: ViewContainerRef })
-  // currentSongComponent!: ViewContainerRef;
   constructor(
     private routingService: RoutingService,
     private locationService: LocationService,
-    private emailService: EmailService // private componentFactoryResolver: ComponentFactoryResolver
+    private emailService: EmailService
   ) {}
 
-  ngOnInit(): void {
-    // this.lazyLoadSpotifyComponent();
-  }
+  ngOnInit(): void {}
 
   ngOnDestroy(): void {
     this.sendSiteVisit();
@@ -47,17 +43,4 @@ export class HomeComponent implements OnInit, OnDestroy {
         });
     }
   }
-
-  // private async lazyLoadSpotifyComponent() {
-  //   const { CurrentSongComponent } = await import(
-  //     "./current-song/current-song.component"
-  //   );
-
-  //   const componentFactory =
-  //     this.componentFactoryResolver.resolveComponentFactory(
-  //       CurrentSongComponent
-  //     );
-  //   this.currentSongComponent.clear();
-  //   this.currentSongComponent.createComponent(componentFactory);
-  // }
 }
