b949392649cfe60bb284ad41d934997b34e30be8
Christian Gracia <christianmgracia@gmail.com>
2025-10-22 21:21:02 -0700

    chore: upgraded angular 18

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, Optional, SkipSelf } from "@angular/core";
-import { HttpClientModule } from "@angular/common/http";
+import {} from "@angular/common/http";
 import { EnsureModuleLoadedOnceGuard } from "./ensure-module-loaded-once.guard";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SharedModule } from "src/app/modules/shared/shared.module";
