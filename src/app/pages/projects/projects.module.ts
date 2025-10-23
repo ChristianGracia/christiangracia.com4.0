4d152bf04bd78980596e512aeaba0464e564986d
Christian Gracia <christianmgracia@gmail.com>
2025-10-22 21:00:03 -0700

    fix: upgrade to angular13

--- src/app/pages/projects/projects.module.ts
+++ src/app/pages/projects/projects.module.ts
@@ -12,21 +12,20 @@ import { MatCarouselModule } from "@ngbmodule/material-carousel";
 import { SharedModule } from "src/app/modules/shared/shared.module";
 
 @NgModule({
-  declarations: [
-    GithubReposComponent,
-    WebsitesComponent,
-    SiteComponent,
-    ProjectsComponent,
-    RepoCommitModalComponent,
-    ViewSiteModalComponent,
-    SafePipe,
-  ],
-  imports: [
-    MatCarouselModule.forRoot(),
-    ProjectsRoutingModule,
-    LazySharedModule,
-    SharedModule,
-  ],
-  entryComponents: [RepoCommitModalComponent, ViewSiteModalComponent],
+    declarations: [
+        GithubReposComponent,
+        WebsitesComponent,
+        SiteComponent,
+        ProjectsComponent,
+        RepoCommitModalComponent,
+        ViewSiteModalComponent,
+        SafePipe,
+    ],
+    imports: [
+        MatCarouselModule.forRoot(),
+        ProjectsRoutingModule,
+        LazySharedModule,
+        SharedModule,
+    ]
 })
 export class ProjectsModule {}
